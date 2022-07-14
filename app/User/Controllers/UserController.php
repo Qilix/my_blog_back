3501efe70bf72af112fe4dca97f44cd403f47aa2
Qilix <leevalenti@mail.ru>
2022-07-14 04:08:09 +0300

    added Auth

---
+++ app/User/Controllers/UserController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\User\Controllers;
+
+use Illuminate\Routing\Controller;
+
+use App\Common\Models\User;
+
+use App\User\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+
+
+
+class UserController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $user = User::create(array_merge(
+            $request->only('name', 'email'),
+            ['password' => bcrypt($request->password)],
+        ));
+
+        return response()->json([
+            'message' => 'You were successfully registered. Use your email and password to sign in.'
+        ], 200);
+    }
+
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'You cannot sign with those credentials',
+                'errors' => 'Unauthorised'
+            ], 401);
+        }
+
+        $token = Auth::user()->createToken(config('app.name'));
+        $token->token->expires_at = $request->remember_me ?
+            Carbon::now()->addMonth() :
+            Carbon::now()->addDay();
+
+        $token->token->save();
+
+        return response()->json([
+            'token_type' => 'Bearer',
+            'token' => $token->accessToken,
+            'expires_at' => Carbon::parse($token->token->expires_at)->toDateTimeString()
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => 'You are successfully logged out',
+        ]);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
@@ -62,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Commons\Models\User::class,
+            'model' => App\Common\Models\User::class,
         ],
 
         // 'users' => [
